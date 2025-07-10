b7523c0766d94bee1725b2ce15f3401bfc4b068a
Aashna Jain <96052967+Aashna06Jain@users.noreply.github.com>
2025-07-10 11:39:55 +0530

    Delete content.js

--- content.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("mouseup", () => {
-    let selectedText = window.getSelection().toString().trim();
-
-    if (selectedText.length > 0){
-        chrome.storage.local.set({ selectedText});
-    }
-});
